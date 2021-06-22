c49a0f3dc5c465b985b1d602186eee1b1808bcf1
Seanmclem <seanmclements@gmail.com>
2021-06-22 17:35:00 -0400

    basic initial login and signup form

---
+++ src/components/LoginSignupPage.tsx
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useForm } from "react-hook-form";
+
+interface props {}
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginFormContainer = styled.div`
+  background-color: lightgray;
+  /* min-height: 300px; */
+  padding: 50px;
+
+  form: {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Label = styled.label`
+  margin-bottom: 5px;
+`;
+
+const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding-bottom: 15px;
+`;
+
+export const LoginSignupPage: React.FC<props> = () => {
+  const [formType, setFormType] = useState<"login" | "signup">("signup");
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    if (formType === "login") {
+      console.log(formType, { ...data });
+    }
+    if (formType === "signup") {
+      console.log(formType, { ...data });
+    }
+  };
+
+  return (
+    <Container>
+      <LoginFormContainer>
+        <NavContainer>
+          <button onClick={() => setFormType("login")}>Login</button>
+          <button onClick={() => setFormType("signup")}>Signup</button>
+        </NavContainer>
+        {formType === "login" && (
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Label htmlFor="email">
+              Email:
+              <input
+                id="email"
+                type="text"
+                {...register("email", { required: true })}
+              />
+            </Label>
+            <input type="submit" value="Login" />
+          </Form>
+        )}
+        {formType === "signup" && (
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Label htmlFor="firstname">
+              First Name:
+              <input
+                id="firstname"
+                type="text"
+                {...register("firstname", { required: true })}
+              />
+            </Label>
+            <Label htmlFor="lastname">
+              Last Name:
+              <input
+                id="lastname"
+                type="text"
+                {...register("lastname", { required: true })}
+              />
+            </Label>
+            <Label htmlFor="email">
+              Email:
+              <input
+                id="email"
+                type="email"
+                {...register("email", { required: true })}
+              />
+            </Label>
+            <Label htmlFor="phone">
+              Phone:
+              <input
+                id="phone"
+                type="text"
+                {...register("phone", { required: true })}
+              />
+            </Label>
+            <Label htmlFor="address">
+              Address:
+              <input
+                id="address"
+                type="text"
+                {...register("address", { required: true })}
+              />
+            </Label>
+            <Label htmlFor="role">
+              Role:
+              <select {...register("role", { required: true })}>
+                <option value="admin">Admin</option>
+                <option value="client">Client</option>
+                <option selected value="user">
+                  User
+                </option>
+              </select>
+            </Label>
+            <Label htmlFor="clientid">
+              Client id:
+              <input
+                id="clientid"
+                type="text"
+                {...register("clientid", { required: true })}
+              />
+            </Label>
+            <input type="submit" value="Signup" />
+          </Form>
+        )}
+      </LoginFormContainer>
+    </Container>
+  );
+};
